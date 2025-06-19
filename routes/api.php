eb7ed6fc23aa03bf523a551c3813042b79702d5d
danaildr <danail@danaildr.eu>
2025-06-19 20:40:24 +0300

    route problem solve

--- routes/api.php
+++ routes/api.php
@@ -26,20 +26,20 @@ Route::prefix('auth')->group(function () {
 });
 
 // Protected routes
-Route::middleware('auth:sanctum')->group(function () {
+Route::middleware('auth:sanctum')->name('api.')->group(function () {
     // Authentication routes
-    Route::prefix('auth')->group(function () {
-        Route::post('logout', [AuthApiController::class, 'logout']);
-        Route::get('user', [AuthApiController::class, 'user']);
-        Route::post('refresh', [AuthApiController::class, 'refresh']);
-        Route::put('profile', [AuthApiController::class, 'updateProfile']);
-        Route::put('password', [AuthApiController::class, 'changePassword']);
-        Route::get('statistics', [AuthApiController::class, 'statistics']);
+    Route::prefix('auth')->name('auth.')->group(function () {
+        Route::post('logout', [AuthApiController::class, 'logout'])->name('logout');
+        Route::get('user', [AuthApiController::class, 'user'])->name('user');
+        Route::post('refresh', [AuthApiController::class, 'refresh'])->name('refresh');
+        Route::put('profile', [AuthApiController::class, 'updateProfile'])->name('profile');
+        Route::put('password', [AuthApiController::class, 'changePassword'])->name('password');
+        Route::get('statistics', [AuthApiController::class, 'statistics'])->name('statistics');
     });
 
     // Bank Accounts
     Route::apiResource('bank-accounts', BankAccountApiController::class);
-    Route::get('bank-accounts/{bankAccount}/statistics', [BankAccountApiController::class, 'statistics']);
+    Route::get('bank-accounts/{bankAccount}/statistics', [BankAccountApiController::class, 'statistics'])->name('bank-accounts.statistics');
 
     // Transactions
     Route::apiResource('transactions', TransactionApiController::class);
@@ -49,11 +49,11 @@ Route::middleware('auth:sanctum')->group(function () {
 
     // Counterparties
     Route::apiResource('counterparties', CounterpartyApiController::class);
-    Route::get('counterparties/{counterparty}/statistics', [CounterpartyApiController::class, 'statistics']);
-    Route::get('counterparties/{counterparty}/transactions', [CounterpartyApiController::class, 'transactions']);
+    Route::get('counterparties/{counterparty}/statistics', [CounterpartyApiController::class, 'statistics'])->name('counterparties.statistics');
+    Route::get('counterparties/{counterparty}/transactions', [CounterpartyApiController::class, 'transactions'])->name('counterparties.transactions');
 
     // Transaction Types
     Route::apiResource('transaction-types', TransactionTypeApiController::class);
-    Route::get('transaction-types/{transactionType}/statistics', [TransactionTypeApiController::class, 'statistics']);
-    Route::get('transaction-types/{transactionType}/transactions', [TransactionTypeApiController::class, 'transactions']);
+    Route::get('transaction-types/{transactionType}/statistics', [TransactionTypeApiController::class, 'statistics'])->name('transaction-types.statistics');
+    Route::get('transaction-types/{transactionType}/transactions', [TransactionTypeApiController::class, 'transactions'])->name('transaction-types.transactions');
 });
